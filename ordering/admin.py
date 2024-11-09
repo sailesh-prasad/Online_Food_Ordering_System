3d5336e75934f260dd42d12ff83f554e900159cf
Saikrishnan3584 <sailesh-prasad>
2024-11-09 15:04:21 +0530

    Ordering module backend

--- ordering/admin.py
+++ ordering/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import FoodItem, Order, OrderItem
 
-# admin.site.site_header = 'My Awesome Admin'
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('customer__username', 'customer__email')
+    inlines = [OrderItemInline] 
+
+class FoodItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description', 'image')
+    search_fields = ('name', 'description')
+    list_filter = ('price',)
+
+admin.site.register(FoodItem, FoodItemAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
