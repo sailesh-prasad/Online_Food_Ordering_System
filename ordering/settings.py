7aa67da7cf6828e7ec5d68d5363fbf926968b45f
sneha2835 <snehakamatam28@gmail.com>
2024-11-10 15:40:54 +0530

    Created a new Database for Food ordering Website

--- ordering/settings.py
+++ ordering/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'ordering.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Use mysqlclient
+        'NAME': 'food_ordering_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -134,3 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'my_files')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+MYSQL_PATH = "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql.exe"  # Update if the path differs
+if os.path.exists(MYSQL_PATH):
+    DATABASES['default']['OPTIONS'] = {'read_default_file': MYSQL_PATH}
