e874db17cb6b89b4101c5e69b3e30eaa354cc7de
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-11-11 18:36:37 +0530

    modified setting.py

--- ordering/settings.py
+++ ordering/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import pymysql
 from pathlib import Path
 import os
 
@@ -91,7 +91,8 @@ DATABASES = {
         
     }
 }
-
+pymysql.version_info = (1, 4, 2, "final", 0)
+pymysql.install_as_MySQLdb()
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
