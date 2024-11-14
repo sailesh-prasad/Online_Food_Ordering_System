73ce19b7074cf182183e689463fca8c164d9a999
piyushgautam26 <piyushgautam1029@gmail.com>
2024-11-14 20:25:46 +0530

    Created models for all apps

--- ordering/settings.py
+++ ordering/settings.py
@@ -34,6 +34,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ordering',
+    'restaurant',
+    'delivery',
     'customer.apps.CustomerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -81,22 +84,23 @@ WSGI_APPLICATION = 'ordering.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',  # Use mysqlclient
+#        'NAME': 'food_ordering_db',
+#        'USER': 'root',
+#        'PASSWORD': 'root',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Use mysqlclient
-        'NAME': 'food_ordering_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -143,5 +147,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'my_files')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
