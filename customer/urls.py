3668f4660d76e804cbd9c0098aec0a73156c4d6f
pythonandspring <springboardmentor0107@gmail.com>
2024-11-19 19:51:34 +0530

    Update urls.py

--- customer/urls.py
+++ customer/urls.py
@@ -2,20 +2,6 @@ from django.urls import path, include
 from .api import urls as api_urls
 
 
-
 urlpatterns = [
     path('api/', include(api_urls)),
 ]
-
-# urlpatterns = [
-#     path('student_list', views.student_list, name='student_list'),
-#     path('studentr-detail/<int:student_id>/', views.student_detail, name='student_detail') 
-# ]
-
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('admin/manage-privileges/', views.manage_admin_privileges, name='manage_admin_privileges'),
-#     path('admin/edit-privilege/<int:user_id>/', views.edit_admin_privilege, name='edit_admin_privilege'),
-# ]
