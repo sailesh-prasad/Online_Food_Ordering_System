0d79900ebfcbc2b9160d6832e8ed048b7a38bfe0
AnjanVankayala <hkanjanv@gmail.com>
2024-12-02 16:31:10 +0530

    updated the models

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from delivery.models import deliveryUser
 
-# Register your models here.
+class DeliveryUserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'active_status', 'location']
+
+admin.site.register(deliveryUser, DeliveryUserAdmin)
--- order/models.py
+++ order/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from menu.models import Menu
+from delivery.models import deliveryUser
 # Create your models here.
+class Order(models.Model):
+    item = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    sum_of_price = models.DecimalField(max_digits=10, decimal_places=2)
+    order_no = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=[('PENDING', 'Pending'), ('CONFIRMED', 'Confirmed'), ('OUT_FOR_DELIVERY', 'Out for Delivery'), ('DELIVERED', 'Delivered'), ('CANCELLED', 'Cancelled')], default='PENDING')
+    delivery_person = models.ForeignKey(deliveryUser, on_delete=models.SET_NULL, null=True, blank=True)
+    restaurant_name = models.CharField(max_length=100)
+    comments = models.TextField(blank=True, null=True)
+    date_time = models.DateTimeField(auto_now_add=True)
