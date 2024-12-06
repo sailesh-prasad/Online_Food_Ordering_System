6432bdfe25d8b0a3c0cf2ea055109daf4b260fb4
SAKSHI KUMARI <sakshi.kumar19902@gmail.com>
2024-12-06 18:56:00 +0530

    Token Authentication Added

--- ordering/settings.py
+++ ordering/settings.py
@@ -104,12 +104,12 @@ REST_FRAMEWORK = {
     ],
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
