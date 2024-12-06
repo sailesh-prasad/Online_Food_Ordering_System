8a95e76271748f1312c0ef8141c14181b3e1e739
SAKSHI KUMARI <122092273+Sakshi-kumari19@users.noreply.github.com>
2024-12-06 19:51:45 +0530

    Token authentication and Swagger added.

--- ordering/settings.py
+++ ordering/settings.py
@@ -105,12 +105,12 @@ REST_FRAMEWORK = {
     ],
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 
 
