6b3bc7f9ff3ec600c0887ac2b71c8a8a8d5ade48
Your New Username <sankarsailesh001@gmail.com>
2024-12-30 20:29:39 +0530

    time zone updated

--- ordering/settings.py
+++ ordering/settings.py
@@ -148,9 +148,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-IN'  # or 'hi-IN' for Hindi language
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'  # Set to India Standard Time (IST)
 
 USE_I18N = True
 
